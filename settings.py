8284ed9415e334c100d7839669a8675bd55f7c78
ʳʸᵏᵉʳ <81254651+rykerxd@users.noreply.github.com>
2021-10-22 16:44:42 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,8 +15,3 @@ TELEGRAM_SUPPORT_CHAT_ID = os.getenv("TELEGRAM_SUPPORT_CHAT_ID")
 if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip("-").isdigit():
     raise Exception("You need to specify 'TELEGRAM_SUPPORT_CHAT_ID' env variable: The bot will forward all messages to this chat_id. Add this bot https://t.me/ShowJsonBot to your private chat to find its chat_id.")
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
-
-
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "")
-REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "")
-WRONG_REPLY = os.getenv("WRONG_REPLY", "")
